93733641e34e411a52924dbe246d2d24bf4695d7
Chloe <144443749+chloeg07@users.noreply.github.com>
2023-11-15 12:08:18 +0000

    Add files via upload

---
+++ best things free.py
@@ -0,0 +1,16 @@
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.\n")
+lst=[ 1, 6, 3, 5, 3, 4 ]
+if 7 in lst:
+  print("7 is in lst")
+else:
+  print("7 is not in lst")
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.\n")
+lst=[ 1, 6, 3, 5, 3, 4 ]
+if 7 not in lst:
+  print("7 is not in lst")
+else:
+  print("7 is in lst")
